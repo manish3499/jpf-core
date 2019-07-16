975b7b3db6240d6b2712ae9a7a3c56206c37d9b3
Manish <manish3499@gmail.com>
2019-07-16 16:20:42 +0530

    Implements the native peer ChoiceCounter

---
+++ src/peers/gov/nasa/jpf/vm/JPF_gov_nasa_jpf_ChoiceCounter.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2014, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All rights reserved.
+ *
+ * The Java Pathfinder core (jpf-core) platform is licensed under the
+ * Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0. 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
+package gov.nasa.jpf.vm;
+
+import gov.nasa.jpf.Config;
+import gov.nasa.jpf.JPF;
+import gov.nasa.jpf.annotation.MJI;
+import gov.nasa.jpf.util.JPFLogger;
+import gov.nasa.jpf.util.event.CheckEvent;
+import gov.nasa.jpf.util.event.EventContext;
+import gov.nasa.jpf.util.event.Event;
+import gov.nasa.jpf.util.event.EventChoiceGenerator;
+import gov.nasa.jpf.util.event.EventTree;
+import gov.nasa.jpf.util.event.NoEvent;
+
+/**
+ * native peer for ChoiceCounter
+ * Initially it is declared in such a manner that it simply counts the choices. If the native peer works perfectly,
+ * it can be modified to check the uniqueness of the choices too.
+ */
+public class JPF_gov_nasa_jpf_ChoiceCounter extends NativePeer {
+
+  private int choiceCount = 0;
+
+  public int getChoiceCount() {
+    return choiceCount;
+  }
+
+  public void incrementChoiceCount() {
+    choiceCount++;
+  }
+
+  @MJI
+  public void countChoice____ (MJIEnv env, int objRef) {
+    incrementChoiceCount();
+  }
+
+  @MJI
+  public int getChoiceCount____I (MJIEnv env, int objRef) {
+    return getChoiceCount();
+  }
+
+
+}
