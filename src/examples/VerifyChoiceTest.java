8f3ce8e58e09e801b28c4d61da5b025905d52a6e
Manish <manish3499@gmail.com>
2019-08-02 10:36:41 +0530

    Adds the jpf example VerifyChoiceTest

---
+++ src/examples/VerifyChoiceTest.java
@@ -0,0 +1,20 @@
+import gov.nasa.jpf.vm.Verify;
+
+public class VerifyChoiceTest {
+	
+	static void main(String[] args) {
+		int m = Verify.getInt(1, 10);
+		System.out.println("m = " + m);
+		int n = Verify.getInt(1, 2);
+		System.out.println("n = " + n);
+//		int o = Verify.getInt(7,8);
+//		System.out.println("o = " + o);
+//		int p = Verify.getInt(15,16);
+//		System.out.println("p = " + p);
+		//System.out.println("m * n =" + m * n);
+		//assert ( (m * n) < 5); // fails (so we get a trace)
+   }
+	
+	
+}
+
